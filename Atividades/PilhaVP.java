8ade47188e5aef3c6d9fac06a135bcbb67a59745
Carol <caroldazevedo52@gmail.com>
2025-09-22 20:28:07 +0000

    error fix

--- Atividades/PilhaVP.java
+++ Atividades/PilhaVP.java
@@ -1,6 +1,5 @@
 public class PilhaVP{
-	public static void main(String[] args) {
-        int[] PilhaArray = new int[100];
+	public static void main(String[] args) {		
 		Integer[] b = new Integer[1];		
 		PilhaArray pp = new PilhaArray(1,0);
 		System.out.println("inserindo");
