da969d8b8bf3f5688cd6523fe86072dace22720a
Carol <caroldazevedo52@gmail.com>
2025-09-22 20:01:47 +0000

    arrumação do esqueleto

--- Atividades/PilhaVP.java
+++ Atividades/PilhaVP.java
@@ -1,7 +1,7 @@
 public class PilhaVP{
 	public static void main(String[] args) {		
 		Integer[] b = new Integer[1];		
-		PilhaArray pp=new PilhaArray(1,0);
+		PilhaArray pp = new PilhaArray(1,0);
 		System.out.println("inserindo");
 		for(int f=0;f<10;f++){
 		  System.out.println(f);		  
@@ -13,18 +13,4 @@ public class PilhaVP{
 			  System.out.println(" - "+pp.pop());
 		}
 	}
-}
-
-public interface Pilha {    
-    public int size();    
-    public boolean isEmpty();
-    public Object top() throws PilhaVaziaExcecao;
-    public void push(Object o);
-    public Object pop() throws PilhaVaziaExcecao;
-}
-
-public class PilhaVaziaExcecao extends RuntimeException {
-	 public PilhaVaziaExcecao(String err){
-	    super(err);	
-	 }   
 }
