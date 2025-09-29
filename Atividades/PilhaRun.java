d6afab6ffefe0752761de18132fea3e96d6a5ace
Carol <caroldazevedo52@gmail.com>
2025-09-29 15:54:07 -0300

    Update PilhaRun.java

--- Atividades/PilhaRun.java
+++ Atividades/PilhaRun.java
@@ -1,15 +1,15 @@
 public class PilhaRun{
 	public static void main(String[] args){	
-		PilhaArray pp = new PilhaArray(1,0);
+		PilhaArray pp = new PilhaArray(10,0, 0, 1);
 		System.out.println("inserindo");
 		for(int f=0;f<10;f++){
 		  System.out.println(f);		  
-		  pp.push(Integer.valueOf(f));
+		  pp.pushvermelho(Integer.valueOf(f));
 		}
 		System.out.println("retirando");
 		for(int f=0;f<10;f++){
 			  System.out.print(f);
-			  System.out.println(" - "+pp.pop());
+			  System.out.println(" - "+pp.popvermelho());
 		}
 	}
 }
