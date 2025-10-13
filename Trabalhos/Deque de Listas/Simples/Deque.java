dc5a3a0a0d4ba68539287b1ecad9c02266b6120e
Carol <caroldazevedo52@gmail.com>
2025-10-13 13:49:24 -0300

    Update Deque.java

--- Trabalhos/Deque de Listas/Simples/Deque.java
+++ Trabalhos/Deque de Listas/Simples/Deque.java
@@ -1,7 +1,7 @@
 public interface Deque{    
     public int size();    
-    public boolean isEmpty();
-    public Object first() throws EFilaVazia;
+    public boolean estaVazia();
+    public Object inicio() throws EFilaVazia;
     public Object enqueue(Object o);
     public Object denqueue() throws EFilaVazia;
 }
