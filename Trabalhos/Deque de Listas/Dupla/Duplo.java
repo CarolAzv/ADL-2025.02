71d191db0a333d7ea3a57fc5d3b03e23544705d3
Carol <caroldazevedo52@gmail.com>
2025-10-15 01:32:39 +0000

    começo do deque duplo

---
+++ Trabalhos/Deque de Listas/Dupla/Duplo.java
@@ -0,0 +1,10 @@
+public interface Duplo{   
+    public int size();
+    public boolean isEmpty();
+    public int first() throws EDequeVazia;
+    public int last() throws EDequeVazia; 
+    public Object inserirInicio(object):
+     object removerInicio():
+    public Object inserirFim(object):
+     object removerFim():
+}
