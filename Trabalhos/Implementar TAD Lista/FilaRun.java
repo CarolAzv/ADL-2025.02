2e23235aeedf26373b69718e18e013d07bee90e7
Carol <caroldazevedo52@gmail.com>
2025-11-11 17:46:52 -0300

    Update FilaRun.java

--- Trabalhos/Implementar TAD Lista/FilaRun.java
+++ Trabalhos/Implementar TAD Lista/FilaRun.java
@@ -1,1 +1,18 @@
-
+public class FilaRun{
+      private static NoInicio s;
+      private static NoFim f;
+      public static void main(String[] args){
+        InicioLista NoInicio = new InicioLista("Começo");
+        FimLista NoFim = new FimLista("Fim");
+        s.setProximo("Fim");
+        f.setAnterior("Fim");
+        System.out.println("Criando deque:");
+        a = no.getProximo();
+        for(int p=0; p<10; p++){
+            System.out.println("Inserindo "+p);
+            while(a.getProximo()!=null){
+                a = a.getProximo();
+            }
+            a.setProximo(p);
+        })
+}
